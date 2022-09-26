073be3e22e000b33eca75ca1c24f7bd0b9ab359b
LeeChoongHwan <choo901@naver.com>
2022-09-26 14:05:01 +0900

    delete menu

--- app/src/main/java/com/todoay/view/profile/ProfileFragment.kt
+++ app/src/main/java/com/todoay/view/profile/ProfileFragment.kt
@@ -1,11 +1,16 @@
 package com.todoay.view.profile
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import com.bumptech.glide.Glide
 import com.todoay.MainActivity.Companion.mainAct
 import com.todoay.R
 import com.todoay.api.domain.profile.ProfileAPI
@@ -57,7 +62,24 @@ class ProfileFragment : Fragment() {
         mBinding?.profileChangepasswordTv?.setOnClickListener {
             Navigation.findNavController(requireView()).navigate(R.id.action_profileFragment_to_changePasswordFragment)
         }
+        
+        //갤러리 이미지 추가
+        mBinding?.profileAddpictureBtn?.setOnClickListener { 
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            intent.type= "image/*"
+
+            val getGallery = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == RESULT_OK) {
+                    Glide.with(this)
+                        .load(it.data!!.data)
+                        .override(200,200)
+                        .into(mBinding?.profileAddpictureBtn!!)
+                }
+            }
+
+        }
 
         return mBinding?.root
     }
+
 }
